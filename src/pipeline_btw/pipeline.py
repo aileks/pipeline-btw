6f5ab5f08bf3fca36e01dc6482bef0d3cbae4884
Kieran Pedrick <kieran@aileks.dev>
2026-09-02 11:46:01 -0400

    basic data loading in pipeline.py

---
+++ src/pipeline_btw/pipeline.py
@@ -0,0 +1,10 @@
+from pipeline_btw.extract.breweries import extract_data
+from pipeline_btw.load.postgres import load_data
+from pipeline_btw.transform.breweries import transform_data
+
+raw_data = extract_data()
+cleaned_data = transform_data(raw_data)
+try:
+    load_data(cleaned_data)
+except Exception as e:
+    print(f"Data loading failed: {e}")
